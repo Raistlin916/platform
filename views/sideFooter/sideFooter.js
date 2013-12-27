e639bd8dabff7f082a6d8b8ab1154363f88bca0f
Kay <raistlin916@gmail.com>
2013-12-27 11:30:24 +0800

    improve ui

---
+++ views/sideFooter/sideFooter.js
@@ -0,0 +1,20 @@
+angular.module('platform')
+.directive('sideFooter', function(){
+  return {
+    restrict: 'E',
+    scope: {},
+    link: function(scope, elem, attr){
+        scope.list = [{
+          title: 'github',
+          url: 'https://github.com/Raistlin916/platform'
+        }, {
+          title: '反馈',
+          url: '/report'
+        }, {
+          title: '关于',
+          url: '/bout'
+        }];
+    },
+    templateUrl : '/partials/sideFooter.html'
+  }
+});
